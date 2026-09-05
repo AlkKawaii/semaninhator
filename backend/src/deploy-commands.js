2417682ce071035bc60a63efcd524484f7793018
Guilherme Alcantara <129176707+AlkKawaii@users.noreply.github.com>
2026-09-05 14:22:20 -0300

    Feat: Now deploy-commands.js can deploy to a specific guild

--- backend/src/deploy-commands.js
+++ backend/src/deploy-commands.js
@@ -6,14 +6,47 @@ const commands = [semaninhaCommand.data.toJSON()];
 
 const rest = new REST().setToken(process.env.DISCORD_BOT_TOKEN);
 
+const args = process.argv.slice(2);
+console.log(`Quantidade de argumentos: ${args.length}`);
+
+const clientId = process.env.DISCORD_CLIENT_ID;
+const guildId  = process.env.DISCORD_GUILD_ID;
+
 try {
-  console.log("Registrando slash commands globalmente...");
+    console.log("Limpando comandos registrados");
 
-  await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
-    body: commands,
-  });
+    console.log("Limpando comandos globais");
+    await rest.put(Routes.applicationCommands(clientId), {
+        body: []
+    });
 
-  console.log("Comandos globais registrados com sucesso.");
+    console.log("Limpando comandos para o servidor");
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: []
+    });
+
+    console.log("Todos os comandos registrados foram limpos");
+    if (args.includes("--global")) {
+        console.log("Registrando slash commands globalmente...");
+
+        await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
+            body: commands,
+        });
+
+        console.log("Comandos globais registrados com sucesso.");
+    } else {
+        console.log("Registrando slash commands para servidor específico...");
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                clientId,
+                guildId
+            ),
+            { body: commands }
+        );
+
+        console.log("Comandos registrados no servidor com sucesso.");
+    }
 } catch (error) {
-  console.error(error);
+    console.error(error);
 }
